895adce81194f91d2dc8a263f86ef14e1bda538e
lewKcoder <7kamimura74@gmail.com>
2023-05-25 01:13:09 +0900

    Fixed block jsx

--- src/components/division-blocks/block/block.tsx
+++ src/components/division-blocks/block/block.tsx
@@ -1,26 +1,27 @@
 import { Component } from './types';
-import { Box, Link } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 
 export const Block: Component = (props) => {
   const { text, backgroundColor, hoverBackgroundColor, left, top, width, height } = props;
   return (
-    <Box left={left && `${left}px`} top={top && `${top}px`} position={'absolute'}>
-      <Link
-        href="#"
-        alignItems={'center'}
-        bgColor={backgroundColor}
-        border={'1px solid #fff'}
-        borderRadius={'8px'}
-        color="#fff"
-        display={'flex'}
-        fontWeight={'bold'}
-        justifyContent={'center'}
-        height={`${height}px`}
-        width={`${width}px`}
-        _hover={{ bg: hoverBackgroundColor, textDecoration: 'none' }}
-      >
-        {text}
-      </Link>
-    </Box>
+    <Link
+      href="#"
+      alignItems={'center'}
+      bgColor={backgroundColor}
+      border={'1px solid #fff'}
+      borderRadius={'8px'}
+      color="#fff"
+      display={'flex'}
+      fontWeight={'bold'}
+      justifyContent={'center'}
+      height={`${height}px`}
+      width={`${width}px`}
+      left={left && `${left}px`}
+      top={top && `${top}px`}
+      position={'absolute'}
+      _hover={{ bg: hoverBackgroundColor, textDecoration: 'none' }}
+    >
+      {text}
+    </Link>
   );
 };
