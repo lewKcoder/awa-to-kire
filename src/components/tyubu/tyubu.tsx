d95638f043c4b6e40c00f0c9c6253fa98a96fbfe
lewKcoder <7kamimura74@gmail.com>
2023-05-25 00:53:14 +0900

    Added tyubu data json

--- src/components/tyubu/tyubu.tsx
+++ src/components/tyubu/tyubu.tsx
@@ -1,5 +1,7 @@
 import { Component } from './types';
+import { DivisionBlocks } from '@/components/division-blocks';
+import data from './data.json';
 
 export const Tyubu: Component = () => {
-  return <div>Tyubu</div>;
+  return <DivisionBlocks {...data} />;
 };
