a45d155ff8c9af05bdec814cbc324050ea515c4b
lewKcoder <7kamimura74@gmail.com>
2023-05-25 22:57:31 +0900

    Added shikoku data json

--- src/components/shikoku/shikoku.tsx
+++ src/components/shikoku/shikoku.tsx
@@ -1,5 +1,7 @@
 import { Component } from './types';
+import { DivisionBlocks } from '@/components/division-blocks';
+import data from './data.json';
 
 export const Shikoku: Component = () => {
-  return <div>Shikoku</div>;
+  return <DivisionBlocks {...data} />;
 };
