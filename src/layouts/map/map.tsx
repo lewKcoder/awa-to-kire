b4a1e1e113989d9d2064ccd15c8985d5fa95df1b
lewKcoder <7kamimura74@gmail.com>
2023-05-31 15:43:17 +0900

    Fixed map component

--- src/layouts/map/map.tsx
+++ src/layouts/map/map.tsx
@@ -1,18 +1,12 @@
-import { Box, Flex } from '@chakra-ui/react';
-import { Sidebar } from '@/components/sidebar';
 import { Japan } from '@/components/japan';
 import { Abroad } from '@/components/abroad';
+import { CommonLayouts } from '../common-layouts';
 
 export const Map: React.FunctionComponent = () => {
   return (
-    <Flex>
-      <Box width={'16%'} borderRight={'1px solid #3a3a3a'}>
-        <Sidebar />
-      </Box>
-      <Box height={'100vh'} width={'84%'} position={'relative'}>
-        <Abroad />
-        <Japan />
-      </Box>
-    </Flex>
+    <CommonLayouts>
+      <Abroad />
+      <Japan />
+    </CommonLayouts>
   );
 };
