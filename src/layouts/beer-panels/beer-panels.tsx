7e9ef1873b507966b78eac628d08d41b6a6db52b
lewKcoder <7kamimura74@gmail.com>
2023-06-02 00:57:53 +0900

    Fixed beer-panels jsx and common-layout styles

--- src/layouts/beer-panels/beer-panels.tsx
+++ src/layouts/beer-panels/beer-panels.tsx
@@ -2,23 +2,26 @@ import { CommonLayouts } from '../common-layouts';
 import { Component } from './types';
 import { Circle } from '@/components/circle';
 import { BeerPanel } from '@/components/beer-panel';
+import { Text, Box } from '@chakra-ui/react';
 
 export const BeerPanels: Component = (props) => {
   const { prefectures } = props;
 
   return (
     <CommonLayouts>
-      <div>
-        <Circle type="image" href="/" src="/beer-list/color-japan.png" alt="ColorJapanIcon" />
+      <Circle type="image" href="/" src="/beer-list/color-japan.png" alt="ColorJapanIcon" />
+      <Box padding={'0 64px'}>
         {prefectures.map(({ prefecture, beers }) => (
-          <>
-            <div key={prefecture}>{prefecture}</div>
+          <Box key={prefecture} marginBottom={'32px'} id={prefecture}>
+            <Text fontWeight={900} fontSize={'22px'} padding={'24px 0'}>
+              {prefecture}
+            </Text>
             {beers.map((beer) => (
               <BeerPanel key={beer.name} {...beer} />
             ))}
-          </>
+          </Box>
         ))}
-      </div>
+      </Box>
     </CommonLayouts>
   );
 };
--- src/layouts/common-layouts/styles.ts
+++ src/layouts/common-layouts/styles.ts
@@ -1,10 +1,12 @@
 export const SidebarBoxStyles = {
   width: '16%',
   borderRight: '1px solid #3a3a3a',
+  position: 'fixed',
+  height: '100%',
 };
 
 export const ChildrenBoxStyles = {
-  height: '100vh',
   width: '84%',
   position: 'relative',
+  left: '16%',
 };
