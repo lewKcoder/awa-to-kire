1111d9c6a6a3b000ebdb555047246315a9bcd311
lewKcoder <7kamimura74@gmail.com>
2023-06-01 21:20:28 +0900

    Fixed Link tag

--- src/components/division-blocks/block/block.tsx
+++ src/components/division-blocks/block/block.tsx
@@ -1,28 +1,30 @@
 import { Component } from './types';
-import { Link } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
+import Link from 'next/link';
 
 export const Block: Component = (props) => {
   const { text, backgroundColor, left, top, width, height, division } = props;
   return (
-    <Link
-      href={`${division}/beers#${text}`}
-      alignItems={'center'}
-      bgColor={'#fff'}
-      border={`3px solid ${backgroundColor}`}
-      borderRadius={'4px'}
-      color="#363636"
-      display={'flex'}
-      fontWeight={'bold'}
-      fontSize={'14px'}
-      justifyContent={'center'}
-      height={`${height}px`}
-      width={`${width}px`}
-      left={left && `${left}px`}
-      top={top && `${top}px`}
-      position={'absolute'}
-      _hover={{ bg: backgroundColor, color: '#fff', textDecoration: 'none' }}
-    >
-      {text}
+    <Link href={`${division}/beers#${text}`}>
+      <Text
+        alignItems={'center'}
+        bgColor={'#fff'}
+        border={`3px solid ${backgroundColor}`}
+        borderRadius={'4px'}
+        color="#363636"
+        display={'flex'}
+        fontWeight={'bold'}
+        fontSize={'14px'}
+        justifyContent={'center'}
+        height={`${height}px`}
+        width={`${width}px`}
+        left={left && `${left}px`}
+        top={top && `${top}px`}
+        position={'absolute'}
+        _hover={{ bg: backgroundColor, color: '#fff', textDecoration: 'none' }}
+      >
+        {text}
+      </Text>
     </Link>
   );
 };
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,27 +1,41 @@
-import { Box, Image, Flex, Text, Link } from '@chakra-ui/react';
+import { Box, Image, Flex, Text } from '@chakra-ui/react';
+import Link from 'next/link';
 
 export const Sidebar: React.FunctionComponent = () => {
   return (
     <Flex flexDirection={'column'}>
-      {/* 仮アイコン */}
-      <Flex flexDirection={'column'} alignItems={'center'} padding={'32px 0'}>
-        <Image src="/header/beer.png" alt="siteIcon" width={'80px'} />
-        <Text fontWeight={900} fontSize={'20px'}>
-          ビール日和
-        </Text>
-      </Flex>
+      <Link href="/">
+        <Flex flexDirection={'column'} alignItems={'center'} padding={'32px 0'}>
+          <Image src="/header/beer.png" alt="siteIcon" width={'80px'} />
+          <Text fontWeight={900} fontSize={'20px'}>
+            ビール日和
+          </Text>
+        </Flex>
+      </Link>
       <Box width={'100%'}>
-        <Link display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
-          <Image src="/header/light-bulb.png" alt="siteIcon" width={'32px'} height={'32px'} />
-          豆知識
+        <Link href="#">
+          <Text display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
+            <Image src="/header/light-bulb.png" alt="siteIcon" width={'32px'} height={'32px'} />
+            ビールの種類
+          </Text>
         </Link>
-        <Link display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
-          <Image src="/header/restaurant.png" alt="siteIcon" width={'32px'} height={'32px'} />
-          おつまみ
+        <Link href="#">
+          <Text display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
+            <Image src="/header/restaurant.png" alt="siteIcon" width={'32px'} height={'32px'} />
+            おつまみ
+          </Text>
         </Link>
-        <Link display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
-          <Image src="/header/cocktail.png" alt="siteIcon" width={'32px'} height={'32px'} />
-          アレンジ
+        <Link href="#">
+          <Text display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
+            <Image src="/header/cocktail.png" alt="siteIcon" width={'32px'} height={'32px'} />
+            アレンジ
+          </Text>
+        </Link>
+        <Link href="#">
+          <Text display={'flex'} alignItems={'center'} gap={'8px'} margin={'24px'}>
+            <Image src="/header/light-bulb.png" alt="siteIcon" width={'32px'} height={'32px'} />
+            豆知識
+          </Text>
         </Link>
       </Box>
     </Flex>
