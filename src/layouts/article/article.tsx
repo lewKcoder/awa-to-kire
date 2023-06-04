78aa4522223f852a37c3f6e4e7e44038ec825de0
lewKcoder <7kamimura74@gmail.com>
2023-06-04 20:09:51 +0900

    Added article component

---
+++ src/layouts/article/article.tsx
@@ -0,0 +1,10 @@
+import { CommonLayouts } from '../common-layouts';
+import { Component } from './types';
+
+export const Article: Component = () => {
+  return (
+    <CommonLayouts>
+      <div>ビールの種類</div>
+    </CommonLayouts>
+  );
+};
