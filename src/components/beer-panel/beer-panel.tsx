38856e67159ef7a385eb1a18cd9bfe5a74f411a4
lewKcoder <7kamimura74@gmail.com>
2023-06-04 19:58:34 +0900

    Fixed beer-panel

--- src/components/beer-panel/beer-panel.tsx
+++ src/components/beer-panel/beer-panel.tsx
@@ -1,12 +1,27 @@
 import { Component } from './types';
+import { Box, Text, Image, Flex } from '@chakra-ui/react';
+import { BeerContents } from '@/components/beer-contents';
+import { containerStyles, prefectureTextStyles, imageStyles, flexGap, fontSize } from './styles';
 
 export const BeerPanel: Component = (props) => {
-  const { name, style, color, alcohol, bitterness, sweetness, fullBodied, feature } = props;
+  const { name, src, alt, style, color, alcohol, bitterness, sweetness, fullBodied, feature } =
+    props;
 
   return (
-    <div>
-      {name}
-      {style}
-    </div>
+    <Box sx={containerStyles} _notLast={{ marginBottom: '24px' }}>
+      <Text sx={prefectureTextStyles}>{name}</Text>
+      <Flex sx={flexGap}>
+        <Image src={src} alt={alt} sx={imageStyles} />
+        <BeerContents
+          style={style}
+          color={color}
+          alcohol={alcohol}
+          bitterness={bitterness}
+          sweetness={sweetness}
+          fullBodied={fullBodied}
+        />
+        <Text sx={fontSize}>{feature}</Text>
+      </Flex>
+    </Box>
   );
 };
---
+++ src/components/beer-panel/styles.ts
@@ -0,0 +1,25 @@
+export const containerStyles = {
+  border: '1px solid',
+  borderRadius: '8px',
+  padding: '24px',
+};
+
+export const prefectureTextStyles = {
+  fontSize: '18px',
+  fontWeight: 900,
+  marginBottom: '8px',
+};
+
+export const imageStyles = {
+  width: '200px',
+  height: 'auto',
+  borderRadius: '4px',
+};
+
+export const flexGap = {
+  gap: '24px',
+};
+
+export const fontSize = {
+  fontSize: '14px',
+};
